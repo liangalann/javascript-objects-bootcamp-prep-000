f40dfe7c08041a724e105217cb5b77226afc4857
Alan Liang <liangalann@gmail.com>
2018-03-19 20:19:40 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,3 +3,7 @@ var playlist = { artistNames : 'songTitles'}
 function updatePlaylist(playlist, artistNames, songTitles) {
   return Object.assign({}, playlist, {[artistNames]: songTitles})
 }
+
+function removeFromPlaylist(playlist, artistNames) {
+  return delete playlist.artistNames
+}
